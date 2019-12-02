4e52508517145da1c4ae25be134b24eeffa5ab00
musteateodora <mustea.teodora@gmail.com>
2019-12-02 14:14:04 +0200

    format post-it

--- post-it/src/PostIt.js
+++ post-it/src/PostIt.js
@@ -3,20 +3,18 @@ import { LitElement, html, css } from "lit-element";
 export class PostIt extends LitElement {
   static get styles() {
     return css`
-    div {
+      div {
         background: #ffff88;
         margin: 25px;
         width: 250px;
-        min-height:250px;
-        max-height:250px;
-        font-family:'Reenie Beanie';
-        font-size:22px;
-        text-align:center;
-      
+        min-height: 250px;
+        max-height: 250px;
+        font-family: "Reenie Beanie";
+        font-size: 22px;
+        text-align: center;
         padding: 10px;
         box-shadow: 5px 10px #888888;
       }
-
     `;
   }
   static get properties() {
@@ -43,8 +41,8 @@ export class PostIt extends LitElement {
     const currentDate = this.date ? new Date().toDateString() : "";
     return html`
       <div>
-  <p><b>${this.title}</b></p>
-  <p><i>${this.description}</i></p>
+        <p><b>${this.title}</b></p>
+        <p><i>${this.description}</i></p>
         <p>${currentDate}</p>
       </div>
     `;
